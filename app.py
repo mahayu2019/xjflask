f09917e18cabce471f1556a71b9f6a6f1669be60
mahayu2019 <2353987275@qq.com>
2019-04-20 19:18:12 +0800

    flask参数

--- app.py
+++ app.py
@@ -5,7 +5,11 @@ from flask import Flask
 __name__表示当前的模块名字
     flask以当前这个模块所在目录为根目录,static为静态目录,templates为模板目录
 '''
-app = Flask(__name__)
+app = Flask(__name__,
+            static_url_path='/static',  # 浏览器中访问静态页面的url前缀,默认static
+            static_folder="static",  # 静态文件的目录,默认static
+            template_folder="templates"  # 模板文件的目录.默认为templates
+            )
 
 
 @app.route('/')
